0797b409fcdb52f3e20caf8af804cc4dd1614180
hikmal <hikmala62@gmail.com>
2022-10-19 14:58:52 -0700

    first commit

---
+++ src/PPH21Calculator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ModernOOP\StudiKasus\PPH21;
+final class PPH21Calculator
+{
+    private $calculators;
+
+    public function __construct(CalculatorInterFace ...$calculators)
+    {
+        $this->calculators = $calculators;
+    }
+    public function calculate(float $pkp): float 
+    {
+        foreach ($this->calculators as $calculator){
+            if ($pkp < $calculator->maxPkp() && $pkp >=$calculator->minPkp()){
+                return $calculator->calculate($pkp);
+            }
+        }
+    }
+}
